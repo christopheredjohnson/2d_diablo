5bed71e234862180117cd937e098750bb3f073e6
Christophere Johnson <cdjohnson0195@gmail.com>
2025-06-06 08:59:50 -0400

    randomize

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -20,15 +21,30 @@ func (s *Spawner) Update(game *Game) {
 		s.Timer = 0
 		s.Spawned++
 
+		var frames []*ebiten.Image
+		var hp int
+
+		switch rand.Intn(3) {
+		case 0:
+			frames = LoadEnemySpriteSheet("assets/slime/green.png", 11, 16, 32)
+			hp = 1
+		case 1:
+			frames = LoadEnemySpriteSheet("assets/slime/red.png", 11, 16, 32)
+			hp = 2
+		case 2:
+			frames = LoadEnemySpriteSheet("assets/bat/default.png", 4, 32, 32)
+			hp = 5
+		}
+
 		enemy := &Enemy{
-			X:          s.X,
-			Y:          s.Y,
-			Speed:      0.5,
-			Frames:     LoadEnemySpriteSheet("assets/slime/green.png", 11, 16, 32),
+			X:          s.X + float64(rand.Intn(40)-20), // slight spawn offset
+			Y:          s.Y + float64(rand.Intn(40)-20),
+			Speed:      1.0,
+			Frames:     frames,
 			FrameDelay: 7,
 			FrameIndex: 0,
 			FrameTimer: 0,
-			HP:         1,
+			HP:         hp,
 		}
 
 		game.Enemies = append(game.Enemies, enemy)
